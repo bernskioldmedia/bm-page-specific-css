7e66d135bd4ab3356182db0491284f50d624e1ad
Erik Bernskiold <erik@bernskioldmedia.com>
2020-07-04 12:03:57 +0200

    Only load for page

--- assets/scripts/src/editor.js
+++ assets/scripts/src/editor.js
@@ -1,7 +1,16 @@
+const { select } = wp.data;
 const { registerPlugin } = wp.plugins;
 import CSSPanel, { PANEL_ICON } from "./components/css-panel";
 
 registerPlugin( "bm-page-specific-css", {
-	render: CSSPanel,
+	render() {
+		const postType = select( "core/editor" ).getCurrentPostType();
+
+		if ( "page" !== postType ) {
+			return null;
+		}
+
+		return <CSSPanel />;
+	},
 	icon: PANEL_ICON
 } );
